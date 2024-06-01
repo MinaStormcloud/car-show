162c41f82431b82a841cafad87662a9fec03ad32
MinaStormcloud <164744381+MinaStormcloud@users.noreply.github.com>
2024-06-01 14:37:28 +0200

    InitialCommit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
+import logo from './logo.svg';
 import './App.css';
-import Layout from './Layout';
-import { BrowserRouter, Routes } from 'react-router-dom';
-import { Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Products from './pages/Products';
-import Contact from './pages/Contact';
-import About from './pages/About';
-import ProfilePage from './pages/ProfilePage';
-import ShoppingCart from './pages/ShoppingCart';
-
-
 
 function App() {
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route index path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/shoppingcart" element={<ShoppingCart/>}/>
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
